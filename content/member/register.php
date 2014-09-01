72f0849f9adb3ddf9d8258daf5219e8e9c7c7749
Seto El Kahfi <setoelkahfi@gmail.com>
2014-09-01 14:42:25 +0700

    Sent email member registration is now working

--- content/member/register.php
+++ content/member/register.php
@@ -1,4 +1,12 @@
 <?php
+function is_registered($email)
+{
+	$sql = "SELECT email
+		FROM member
+		WHERE email = '$email'";
+	$Q	= mysql_query($sql);
+	return mysql_num_rows($Q) > 0 ? true : false;
+}
 function do_register($nama, $email, $password)
 {
 	$sql 	= "INSERT INTO member (nama, email, password)
@@ -9,6 +17,24 @@ function do_register($nama, $email, $password)
 		$_SESSION['member_id'] 		= mysql_insert_id();
 		$_SESSION['member_nama']	= $nama;
 		$_SESSION['member_email'] 	= $email;
+		$body_mail = '<html><head><meta http-equiv="content-type" content="text/html; charset=utf-8" /></head><body>
+<h1>Terimakasih telah mendaftar menjadi member.</h1><p>Silahkan login menggunakan akun Anda.</p>
+Email : '.$email.'<br/>
+Password: '.$password.'<br/>
+<p>Silahkan login <a href="http://cvtungguljati.com/member/login">disini</a>.</p>
+</body>
+</html>';
+		$headers = "From: admin@cvtungguljati.com\r\n";
+		$headers .= "Reply-to: admin@cvtungguljati.com\r\n";
+		$headers .= "Content-type: text/html";
+		if ((isset($_SERVER['HTTP_HOST']) && ($_SERVER['HTTP_HOST'] == 'www.cvtungguljati.com' || $_SERVER['HTTP_HOST'] == 'cvtungguljati.com')) || $_SERVER['SERVER_NAME'] == 'www.cvtungguljati.com' || $_SERVER['SERVER_NAME'] == 'cvtungguljati.com')
+		{
+			$mail_sent = mail($email, "Registrasi Member - CV Tunggul Jati", $body_mail, $headers);
+		}
+		else
+		{
+			echo 'Maaf, saat ini sistem sedang offline. Coba lagi nanti.<br/>';
+		}
 		header('Location: '.BASE_URL.'member');
 	}
 	else
@@ -34,10 +60,25 @@ if ($_POST)
 		$error 		= true;
 		$e_email 	= 'Anda belum mengisi e-mail';
 	}
+	elseif (!filter_var($_POST['email'], FILTER_VALIDATE_EMAIL))
+	{
+		$error 		= true;
+		$e_email 	= 'Format Email salah.';
+	}
+	elseif (is_registered($_POST['email']))
+	{
+		$error 		= true;
+		$e_email 	= 'Email sudah terdaftar. Silahkan Login.';
+	}
 	if ($_POST['password'] == '')
 	{
 		$error 		= true;
 		$e_password = 'Anda belum mengisi password';
+	}
+	elseif (strlen($_POST['password']) < 6)
+	{
+		$error 		= true;
+		$e_password = 'Password minimal 6 karakter.';
 	}
 	if (!$error)
 	{
@@ -51,18 +92,16 @@ if ($_POST)
 			<form name="form" method="POST" action="">
 				<table>
 					<tr>
-						<?php echo isset($e_login) ? '<span class="error">'.$e_login.'</span>' : ''; ?>
-						<td>Nama</td>
-						<td> : <input type="text" name="nama" /></td>
+						<td valign="top">Nama</td>
+						<td valign="top"> : <input type="text" name="nama" value="<?php echo isset($_POST['nama']) ? $_POST['nama'] : '' ; ?>"/><br/><?php echo isset($e_nama) ? '<p class="error">'.$e_nama.'</p>' : ''; ?></td>
 					</tr>
 					<tr>
-						<?php echo isset($e_login) ? '<span class="error">'.$e_login.'</span>' : ''; ?>
-						<td>Email</td>
-						<td> : <input type="text" name="email" /></td>
+						<td valign="top">Email</td>
+						<td valign="top"> : <input type="text" name="email" value="<?php echo isset($_POST['email']) ? $_POST['email'] : '' ; ?>"/><br/><?php echo isset($e_email) ? '<p class="error">'.$e_email.'</p>' : ''; ?></td>
 					</tr>
 					<tr>
-						<td>Password</td>
-						<td> : <input type="password" name="password" /></td>
+						<td valign="top">Password</td>
+						<td valign="top"> : <input type="password" name="password" /><br/><?php echo isset($e_password) ? '<p class="error">'.$e_password.'</p>' : ''; ?></td>
 					</tr>
 					<tr>
 						<td colspan="2" align="center">
