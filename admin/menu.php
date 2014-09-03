d77a6ec41c25afe5b7ebc5849f6ea00f712eba39
Seto El Kahfi <setoelkahfi@gmail.com>
2014-09-03 11:39:46 +0700

    Remove admin vendor. We're not using it anymore

--- admin/menu.php
+++ admin/menu.php
@@ -2,6 +2,5 @@
 <dd><a href='media.php?module=order'>&rArr; Order </a></dd>
 <dd><a href='media.php?module=produk'>&rArr; Produk </a></dd>
 <dd><a href='media.php?module=kategori'>&rArr; Kategori </a></dd>
-<dd><a href='media.php?module=vendor'>&rArr; Vendor </a></dd>
 <dd><a href='media.php?module=hubungi'>&rArr; Hubungi Kami </a></dd>
 <dd><a href='media.php?module=ongkoskirim'>&rArr; Ongkos Kirim </a></dd>
